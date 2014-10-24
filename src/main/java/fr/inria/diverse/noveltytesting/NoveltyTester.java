1b0603b31d5b96f6b3cf3def6c26bba1aeab0227
Maxime Tricoire <max.tricoire@gmail.com>
2014-10-24 14:46:10 +0200

    update

--- src/main/java/fr/inria/diverse/noveltytesting/NoveltyTester.java
+++ src/main/java/fr/inria/diverse/noveltytesting/NoveltyTester.java
@@ -1,57 +1,45 @@
 package fr.inria.diverse.noveltytesting;
 
 import fr.inria.diverse.noveltytesting.model.Interface;
+import fr.inria.diverse.noveltytesting.model.MethodOutput;
+import fr.inria.diverse.noveltytesting.modelgeneration.ModelGeneration;
+import fr.inria.diverse.noveltytesting.modelgeneration.ModelGenerationImpl;
 import fr.inria.diverse.noveltytesting.visitor.InputOutputVisitor;
 import org.kevoree.annotation.*;
 import org.kevoree.annotation.ComponentType;
 import org.kevoree.api.Callback;
-import org.kevoree.api.Context;
-import org.kevoree.api.ModelService;
 import org.kevoree.api.Port;
 
 @ComponentType
 public class NoveltyTester {
-
-    private NoveltyGeneration generator = new NoveltyGenerationImpl();
-
-    @KevoreeInject
-    private Context context;
-
-    @KevoreeInject
-    private ModelService modelService;
 
     @Output
     private Port remoteService;
 
     @Start
     public void start() {
-        NoveltyGeneration gen = new NoveltyGenerationImpl();
+        ModelGeneration gen = new ModelGenerationImpl();
         try {
             Class<?> clazz = Class.forName("org.kevoree.library.Calculator");
             Interface i = gen.generateModel(clazz);
             gen.generateData(i);
             i.accept(new InputOutputVisitor());
 
-            i.getMethods().forEach(m -> {
-                for (Object o : m.getParametersValue()) {
-                    System.out.println("param value= "+o);
-                    System.out.println("param type= "+o.getClass().getTypeName());
+            i.getMethods().forEach(m -> remoteService.call(m.getParametersValue(), new Callback() {
+                @Override
+                public void onSuccess(Object o) {
+                    MethodOutput mo = new MethodOutput();
+                    mo.setReturnVal(o);
+                    m.addMethodOutput("foo", mo);
                 }
 
-                remoteService.call(m.getParametersValue(), new Callback() {
-                    @Override
-                    public void onSuccess(Object o) {
-                        m.setReturnVal(o);
-                    }
+                @Override
+                public void onError(Throwable throwable) {
+                    //m.setThrownException(throwable);
+                }
+            }));
 
-                    @Override
-                    public void onError(Throwable throwable) {
-                        //m.setThrownException(throwable);
-                    }
-                });
-            });
-
-        } catch (ClassNotFoundException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
